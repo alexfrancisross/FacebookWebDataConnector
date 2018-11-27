e4ef079dacad44e8024f8e32f237a2cd777bfb19
alexfrancisross <alexfrancisross@hotmail.com>
2018-11-27 11:24:16 +0000

    Update pagefeedwebconnect.js

--- js/pagefeedwebconnect.js
+++ js/pagefeedwebconnect.js
@@ -1,7 +1,7 @@
 
         var APP_ID = '1445370055764391'; //Facebook APP ID
         var LIMIT = 100; //Number of records to bring back at any time
-        var BASEURL = 'http://files.tableaujunkie.com/facebooksearch/' //base url of virtual directory
+        var BASEURL = 'https://alexfrancisross.github.io/FacebookWebDataConnector/' //base url of virtual directory
 
         window.fbAsyncInit = function() {
             FB.init({
